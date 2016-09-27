ef7f5ae75ebae6fee2067ffb3e6b5ef59f99724b
nilesh.kashid <nilesh.kashid@ril.com>
2016-09-27 18:43:13 +0530

    service changes.

---
+++ app/src/main/java/com/example/nileshkashid/locateme/LocationService.java
@@ -0,0 +1,94 @@
+package com.example.nileshkashid.locateme;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationSettingsResult;
+
+/**
+ * Created by Nilesh.Kashid on 27-09-2016.
+ */
+public class LocationService extends Service implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener,ResultCallback<LocationSettingsResult>
+{
+
+    /** indicates how to behave if the service is killed */
+    int mStartMode;
+
+    /** interface for clients that bind */
+    IBinder mBinder;
+
+    /** indicates whether onRebind should be used */
+    boolean mAllowRebind;
+
+    /** Called when the service is being created. */
+    @Override
+    public void onCreate() {
+
+    }
+
+    /** The service is starting, due to a call to startService() */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return mStartMode;
+    }
+
+    /** A client is binding to the service with bindService() */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /** Called when all clients have unbound with unbindService() */
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return mAllowRebind;
+    }
+
+    /** Called when a client is binding to the service with bindService()*/
+    @Override
+    public void onRebind(Intent intent) {
+
+    }
+
+    /** Called when The service is no longer used and is being destroyed */
+    @Override
+    public void onDestroy() {
+
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(LocationSettingsResult locationSettingsResult) {
+
+    }
+}
